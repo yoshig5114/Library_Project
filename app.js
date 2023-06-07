fb80abd7212488abb7459cc85283af50f72f385f
Joshua Gregory <yoshig5114@gmail.com>
2023-06-06 20:06:33 -0400

    working on markRead method of Library

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-console.log("Hello World!\n==========\n");
 
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
+class Book{
+    constructor(id, title, author, read){
+        this.id = id
+        this.title = title
+        this.author = author
+        this.read = read
+    }
+}
+class Library{
+    constructor(bookCount, books){
+        this.bookCount = bookCount
+        this.books = books 
+    }
+    markRead(checkbox, id){
+        
+    }
+}
