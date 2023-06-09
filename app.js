8ca7eb0a370aba88bd1afa9375d9567d30d5838d
Joshua Gregory <yoshig5114@gmail.com>
2023-06-08 20:44:05 -0400

    finished attempt, seems to work

--- app.js
+++ app.js
@@ -28,9 +28,28 @@ class Library{
         let authorInput = document.querySelector("#authorInput")
         let authorValue = authorInput.value
         let readCheckboxInput = document.querySelector("#readCheckbox")
-        let readCheckboxValue = readCheckboxInput.value
+        let readCheckboxValue = readCheckboxInput.checked
         let NewBook = new Book(titleValue, authorValue, readCheckboxValue)
-        
-
+        this.books.push(NewBook)
+        let newTr = document.createElement("tr")
+        let titleCell = document.createElement("td")
+        titleCell.textContent = titleValue
+        let authorCell = document.createElement("td")
+        authorCell.textContent = authorValue
+        let readCell = document.createElement("td")
+        let readCheckbox = document.createElement("input");
+        readCheckbox.type = "checkbox";
+        readCheckbox.checked = readCheckboxValue;
+        readCheckbox.disabled = true;
+        readCell.appendChild(readCheckbox);
+        newTr.appendChild(titleCell)
+        newTr.appendChild(authorCell)
+        newTr.appendChild(readCell)
+        let tableBody = document.querySelector("#tableBody")
+        tableBody.appendChild(newTr)
+        this.bookCount = this.books.length
     }
 }
+let newLibrary = new Library(1, [])
+let addBookButton = document.querySelector("#addBookButton")
+addBookButton.addEventListener("click", function(){newLibrary.addBook()})
